ce4c706995cd270b423fd2dfeb13afb61018c9a3
kurubaoffice <kurubaoffice@gmail.com>
2025-07-23 22:17:44 +0530

    Todder2.0 Started

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Tidder2.0')
+    print_hi('Welcome to Tidder2.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
